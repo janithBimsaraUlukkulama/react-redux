14e1d96f285f48bf919c14ec779d1d36ef2989f4
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2021-12-12 00:33:25 +0530

    basics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { createStore } from 'redux';
+
+// Store -> glabalized state
+
+// Action 
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// Reducer - how action transform to next state
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+let store = createStore(counter);
+store.subscribe(() => console.log(store.getState()));
+
+// Dispatch 
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
